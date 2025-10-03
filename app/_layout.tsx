47bd50cdb65cb1a14f7f58df9bc3af00c913e132
Alex Georgiev <georgievalex114@gmail.com>
2025-10-03 10:42:51 +0300

    GH-3 Implement navigation.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,34 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import {Stack} from 'expo-router';
 import 'react-native-reanimated';
+import {ClerkProvider, useAuth} from "@clerk/clerk-expo";
+import {tokenCache} from "@clerk/clerk-expo/token-cache";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+function RootLayoutWithAuth() {
+    const {isSignedIn, isLoaded} = useAuth();
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+    if (!isLoaded) {
+        return null;
+    }
+
+    return (
+        <Stack>
+            <Stack.Protected guard={isSignedIn}>
+                <Stack.Screen name='(protected)'/>
+            </Stack.Protected>
+            <Stack.Protected guard={!isSignedIn}>
+                <Stack.Screen name='(public)'/>
+            </Stack.Protected>
+        </Stack>
+    )
+}
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    return (
+        <ClerkProvider tokenCache={tokenCache}
+                       publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+        >
+            <RootLayoutWithAuth/>
+        </ClerkProvider>
+    );
 }
