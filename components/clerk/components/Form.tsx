600a8c0875950bb89d376495448eb42227f10cb4
Alex Georgiev <georgievalex114@gmail.com>
2025-10-03 13:31:53 +0300

    GH-7 Create gradient component.

--- components/clerk/components/Form.tsx
+++ components/clerk/components/Form.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet } from "react-native"
+import Gradient from "@/components/gradient";
 
 interface FormProps {
   title: string
@@ -9,18 +10,21 @@ interface FormProps {
 
 export function Form({ title, subtitle, children, headerChildren }: FormProps) {
   return (
-    <View style={styles.container}>
-      <View style={styles.contentWrapper}>
-        <View style={styles.headerContainer}>
-          <Text style={styles.headerTitle}>{title}</Text>
-          {subtitle && <Text style={styles.headerSubtitle}>{subtitle}</Text>}
-          {headerChildren}
+      <>
+        <View style={styles.container}>
+          <Gradient isSpeaking={false} position='top'/>
+          <View style={styles.contentWrapper}>
+            <View style={styles.headerContainer}>
+              <Text style={styles.headerTitle}>{title}</Text>
+              {subtitle && <Text style={styles.headerSubtitle}>{subtitle}</Text>}
+              {headerChildren}
+            </View>
+            <View style={styles.formContainer}>
+              {children}
+            </View>
+          </View>
         </View>
-        <View style={styles.formContainer}>
-          {children}
-        </View>
-      </View>
-    </View>
+      </>
   )
 }
 
@@ -41,7 +45,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#ffffff",
+    // backgroundColor: "#ffffff",
   },
   headerTitle: {
     fontSize: 32,
@@ -60,7 +64,7 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   formContainer: {
-    backgroundColor: "#ffffff",
+    // backgroundColor: "#ffffff",
     paddingHorizontal: 24,
     paddingTop: 20,
     paddingBottom: 20,
